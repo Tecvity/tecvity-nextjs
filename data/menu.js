87822808c3896613711ace64d2db308b473b49d5
Najam Ul Saqib <najamulsaqib@tutamail.com>
2024-09-14 19:47:22 +0500

    update services page menu item

--- data/menu.js
+++ data/menu.js
@@ -19,21 +19,22 @@ export const menuItems = [
     title: "PAGES",
     subMenuItems: [
       { id: 21, label: "About Page", link: "/about" },
-      {
-        id: 22,
-        title: "Service Page",
+      { id: 22, label: "Services", link: "/service" },
+      // {
+      //   id: 22,
+      //   title: "Service Page",
 
-        subMenuItems: [
-          { id: 221, label: "Service Version 1", link: "/service" },
-          { id: 222, label: "Service Version 2", link: "/service-2" },
-          { id: 223, label: "Service Version 3", link: "/service-3" },
-          {
-            id: 224,
-            label: "Service Details Page",
-            link: "/service-details/2",
-          },
-        ],
-      },
+      //   subMenuItems: [
+      //     { id: 221, label: "Service Version 1", link: "/service" },
+      //     { id: 222, label: "Service Version 2", link: "/service-2" },
+      //     { id: 223, label: "Service Version 3", link: "/service-3" },
+      //     {
+      //       id: 224,
+      //       label: "Service Details Page",
+      //       link: "/service-details/2",
+      //     },
+      //   ],
+      // },
       { id: 23, label: "Team Page", link: "/team" },
       { id: 24, label: "Team Details Page", link: "/team-details/2" },
       { id: 25, label: "Pricing Page", link: "/pricing" },
