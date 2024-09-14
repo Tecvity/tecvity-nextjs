2a26e2003e4ea25855147c378dfa936e4c6b72f3
Najam Ul Saqib <najamulsaqib@tutamail.com>
2024-09-14 21:58:40 +0500

    update header icon

--- components/headers/Header3.jsx
+++ components/headers/Header3.jsx
@@ -114,9 +114,9 @@ export default function Header3() {
                   <div className="header-logo">
                     <Link scroll={false} href="/">
                       <Image
-                        width={86}
-                        height={24}
-                        src="/assets/img/logo.svg"
+                        width={200}
+                        height={64}
+                        src="/assets/img/tecvity-logo.png"
                         alt="logo"
                       />
                     </Link>
