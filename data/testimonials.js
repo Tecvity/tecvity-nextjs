3b61e036fc8bab0420d3ccd4bc0d081685bbdfbd
Najam Ul Saqib <najamulsaqib@tutamail.com>
2024-09-15 08:52:32 +0500

    add testimonials

--- data/testimonials.js
+++ data/testimonials.js
@@ -1,22 +1,16 @@
 export const testimonialData = [
   {
     id: 1,
-    text: "It’s a pleasure working with Bunker. They understood our new brand positioning guidelines and translated them beautifully and consistently into our on-going marketing comms. The team is responsive, quick and always willing help winning partnership",
-    name: "Daniyel Karlos",
-    designation: "Senior Director of Marketing",
+    text: "Najam from the Tecvity team has provided excellent service to secure our Azure environment and application. His deep understanding of application security and Azure, together with his willingness to go the extra mile, made this project very successful, even more than we anticipated. Every cent was worth it, money well spent! I can recommend any company to work with Najam and the Tecvity team as they are very professional, and providing excellent quality!",
+    name: "Attila Horvath",
+    designation: "CEO, Security Consultants",
   },
   {
     id: 2,
-    text: "It’s a pleasure working with Bunker. They understood our new brand positioning guidelines and translated them beautifully and consistently into our on-going marketing comms. The team is responsive, quick and always willing help winning partnership",
-    name: "Daniyel Karlos",
-    designation: "Senior Director of Marketing",
-  },
-  {
-    id: 3,
-    text: "It’s a pleasure working with Bunker. They understood our new brand positioning guidelines and translated them beautifully and consistently into our on-going marketing comms. The team is responsive, quick and always willing help winning partnership",
-    name: "Daniyel Karlos",
-    designation: "Senior Director of Marketing",
-  },
+    text: "Najam and Tecvity have been fantastic to work with.  They are highly skilled and knowledgable and we will continue to engage with them on future security projects.",
+    name: "Brent Glover",
+    designation: "Product Manager, Mach7t",
+  }
 ];
 export const testimonialData2 = [
   {
