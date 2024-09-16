b6c31113693b60c42f9a237898f5de08a3b5b5de
Najam Ul Saqib <najamulsaqib@tutamail.com>
2024-09-16 12:07:00 +0500

    update blog title

--- app/(blog)/blog/page.jsx
+++ app/(blog)/blog/page.jsx
@@ -1,4 +1,4 @@
-import BlogList1 from "@/components/blog/BlogList1";
+import BlogList2 from "@/components/blog/BlogList2";
 import Breadcumb from "@/components/blog/Breadcumb";
 import MarqueeComponent from "@/components/common/Marquee";
 
@@ -7,14 +7,14 @@ import Header3 from "@/components/headers/Header3";
 import React from "react";
 
 export const metadata = {
-  title: "Blog 1 || Frisk - Creative Agency & Portfolio Nextjs Template",
+  title: "Blog | Tecvity",
 };
-export default function BlogPage1() {
+export default function BlogPage2() {
   return (
     <>
       <Header3 />
       <Breadcumb />
-      <BlogList1 />
+      <BlogList2 />
       <MarqueeComponent />
       <Footer8 />
     </>
