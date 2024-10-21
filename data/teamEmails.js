8218a9ddfaa293e053c5241a46a855f65dd24b16
saadamir3180 <saadamir318@gmaill.com>
2024-10-21 13:37:58 +0500

    integrate email subscription
    edit teamEmails to direct emails to the realvent emailUsers

--- data/teamEmails.js
+++ data/teamEmails.js
@@ -2,18 +2,16 @@ const teamEmails =
 [
     "najam@tecvity.co", 
     "moeez@tecvity.co", 
-    "tayyub@tecvity.co"
+    "tayyub@tecvity.co",
+    "saadamir318@gmail.com"
 ];//can only be used after verification
 
 const ownerEmails = 
 [
-    // "najam@tecvity.co", //can only be used after verification
-    "saadamir318@gmail.com",
-    "pixeles3180@gmail.com"
+    "najam@tecvity.co"
 ];
 
-const sourceEmail =  "pixeles3180@gmail.com";
-// const sourceEmail = "hello@tecvity.co";
+const sourceEmail =  "hello@tecvity.co";
 //can only be used after verification
 
 export { teamEmails, ownerEmails, sourceEmail };
