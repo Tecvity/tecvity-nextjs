a821093c8d28754aef3595f06bad173f0167b2b0
Najam Ul Saqib <najamulsaqib@tutamail.com>
2025-01-13 11:12:38 +0500

    update vision

--- components/about/components/Vision.jsx
+++ components/about/components/Vision.jsx
@@ -2,15 +2,36 @@ export default function Vision() {
     return (
       <div className="breadcumb-wrapper">
         <div className="vision-container w-50 mx-auto">
-         <h2 className="title">Our Vision</h2>
-         <br></br>
-         <p className="vision-content text-center fs-4">
-  A just cause for a company refers to the fundamental purpose or mission that drives the organization, 
-  beyond just making profits. It represents a higher sense of meaning or social responsibility, 
-  guiding the company's actions, decisions, and strategies. A just cause is often aligned with values, 
-  ethics, and a vision of creating positive impact in the world.
-</p>
-         </div>
+          <h2 className="title">Our Vision</h2>
+          <br></br>
+          <p className="vision-content text-center fs-4">
+            A just cause for a company refers to the fundamental purpose or
+            mission that drives the organization, beyond just making profits. It
+            represents a higher sense of meaning or social responsibility,
+            guiding the company's actions, decisions, and strategies. A just
+            cause is often aligned with values, ethics, and a vision of creating
+            positive impact in the world.
+          </p>
+          <h3 className="title">Our Guiding Values</h3>
+          <ul className="vision-content fs-4">
+            <li>
+              Inclusivity and Equality: Creating opportunities for everyone to
+              contribute and succeed.
+            </li>
+            <li>
+              Excellence and Innovation: Pushing boundaries and setting
+              benchmarks in technology and service.
+            </li>
+            <li>
+              Integrity and Respect: Ensuring fairness, honesty, and care in
+              every interaction.
+            </li>
+            <li>
+              Social Responsibility: Giving back to the community and supporting
+              sustainable growth.
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
