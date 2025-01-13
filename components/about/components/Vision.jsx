4c5022b0b7a7d15d18dd6fd234853603dfcae230
Najam Ul Saqib <najamulsaqib@tutamail.com>
2025-01-13 11:11:07 +0500

    update vision

--- components/about/components/Vision.jsx
+++ components/about/components/Vision.jsx
@@ -1,16 +1,24 @@
 export default function Vision() {
     return (
       <div className="breadcumb-wrapper">
-        <div className="vision-container w-50 mx-auto">
-         <h2 className="title">Our Vision</h2>
-         <br></br>
-         <p className="vision-content text-center fs-4">
-  A just cause for a company refers to the fundamental purpose or mission that drives the organization, 
-  beyond just making profits. It represents a higher sense of meaning or social responsibility, 
-  guiding the company's actions, decisions, and strategies. A just cause is often aligned with values, 
-  ethics, and a vision of creating positive impact in the world.
-</p>
-         </div>
+      <div className="vision-container w-50 mx-auto">
+        <h2 className="title">Our Vision</h2>
+        <br></br>
+        <p className="vision-content text-center fs-4">
+        At Tecvity, we believe in the power of opportunity. Our mission is
+        to empower individuals—regardless of their background, knowledge, or
+        circumstances—to learn, grow, and achieve their dreams. We are
+        building an ecosystem where ambition meets possibility, innovation
+        thrives, and success knows no limits.
+        </p>
+        <h3 className="title">Our Guiding Values</h3>
+        <ul className="vision-content fs-4">
+        <li>Inclusivity and Equality: Creating opportunities for everyone to contribute and succeed.</li>
+        <li>Excellence and Innovation: Pushing boundaries and setting benchmarks in technology and service.</li>
+        <li>Integrity and Respect: Ensuring fairness, honesty, and care in every interaction.</li>
+        <li>Social Responsibility: Giving back to the community and supporting sustainable growth.</li>
+        </ul>
+      </div>
       </div>
     );
   }
