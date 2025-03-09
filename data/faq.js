ef129a78011a904f103d879b0d0b7551ed19365e
tayyubA <tayyubahmed14@gmail.com>
2025-03-10 01:29:57 +0500

    logo & navbar updated

--- data/faq.js
+++ data/faq.js
@@ -4,33 +4,43 @@ export const faqsHome = [
     question: `UI/UX Design
 `,
     answer: `We craft intuitive and visually stunning interfaces that enhance user experience. Our designs ensure your audience enjoys seamless interaction with your product.`,
+    link: "/our-services/ui_ux-design",
   },
   {
     id: 2,
     question: `Brand Identity Design
 `,
     answer: `We create compelling brand identities that resonate with your audience. A strong, cohesive brand sets you apart and builds trust with your customers.`,
+    link: "/our-services/brand-identity-design",
   },
   {
     id: 3,
     question: `Penetration Testing`,
     answer: `Our thorough vulnerability assessment & penetration testing services identify security weaknesses and help you fortify your system against potential threats, ensuring robust cybersecurity`,
+    link: "/our-services/penetration-testing-&-vulnerability-assessment",
   },
   {
     id: 4,
     question: `Cloud Security Audits`,
     answer: `We conduct security audits and provide recommendations on hardening your cloud security.`,
+    link: "/our-services/cloud-security-audits",
+
+    
   },
 
   {
     id: 5,
     question: `Web & Mobile App Development`,
     answer: `We develop high-performing, scalable web and mobile apps that deliver a superior user experience, ensuring your business stays ahead in the digital age.`,
+    link: "/our-services/web-&-mobile-application-development",
+
   },
   {
     id: 6,
     question: `DevSecOps`,
     answer: `We integrate security into your development pipelines, ensuring secure software delivery.`,
+    link: "/our-services/devsecops",
+
   },
 ];
 
