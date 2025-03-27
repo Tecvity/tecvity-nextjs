a331b79b21af13dfae3a558b5de4e8dc8a5aeae0
mhamzakhattak <hkhattak972@gmail.com>
2025-03-27 15:04:16 +0000

    Added new blog CVE-2025-24016

--- data/categories-tags.js
+++ data/categories-tags.js
@@ -2,11 +2,16 @@
 // Relational linking may be required for this data
 export const categories = [
   { id: 1, text: "Company" },
+  { id: 2, text: "vulnerability" },
 ];
 export const tags = [
   { id: 1, text: "Agency" },
   { id: 2, text: "Technology" },
-  { id: 3, text: "Business" },
+  { id: 3, text: "CVE" },
+  { id: 4, text: "VulnerabilityResearch" },
+  { id: 5, text: "InfoSec" },
+  { id: 6, text: "Exploits" },
+  { id: 7, text: "TechnicalAnalysis" },
 ];
 export const portfolioType = [
   { id: 1, text: "vapt" },
